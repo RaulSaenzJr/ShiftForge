39646fad476aaee038ac67ff76e64bd883e22174
RaulSaenzJr <raul.saenz2@gmail.com>
2026-04-26 18:52:26 -0500

    fixed bug in api.ts causing log in failures

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,8 +1,6 @@
 import axios, { AxiosInstance, InternalAxiosRequestConfig } from 'axios';
 
-const API_URL = import.meta.env.VITE_API_URL
-  ? `${import.meta.env.VITE_API_URL}/api`
-  : '/api';
+const API_URL = import.meta.env.VITE_API_URL || '/api';
 
 const api: AxiosInstance = axios.create({
   baseURL: API_URL,
